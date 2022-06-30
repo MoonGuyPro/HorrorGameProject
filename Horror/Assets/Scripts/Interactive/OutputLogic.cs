fbd8a6d092c4c77bdf9b27580b0c3cf0e275abc9
LiptonLeon <krystian.baraniecki@gmail.com>
2022-06-30 16:57:59 +0200

    feat: More statements in OutputLogic

--- Horror/Assets/Scripts/Interactive/OutputLogic.cs
+++ Horror/Assets/Scripts/Interactive/OutputLogic.cs
@@ -5,36 +5,46 @@ using UnityEngine;
 // Base class for any output object (ex. door, platform)
 public abstract class OutputLogic : MonoBehaviour
 {
+    public enum ops { AND, OR, XOR };
+
+    [Header("Input condition")]
+    public ops op = ops.AND;    // Logical operation
+    public bool invert = false; // Invert result
+
+    [Header("Connected inputs")]
     public List<InputLogic> inputs;
+
+    // Hold info about current state (active/not-active)
     protected bool active;
+
+    // Output behavior after checking current state (ex. animation)
+    // Implement in extended class
+    protected abstract void behavior();
 
     public void Start()
     {
         connectOutputs();
     }
 
-    // Output behavior after checking current state (ex. animation)
-    // Implement in extended class
-    protected abstract void behavior();
-
     // Called by InputLogic.toggle() to update output state
-    // Only AND statement for now
     public void checkState()
     {
-        // If any input is inactive, then output is inactive as well
-        foreach(InputLogic input in inputs)
+        // Check current state using chosen statement
+        switch (op)
         {
-            if (!input.active)
-            {
-                active = false;
-                behavior(); // Call output behavior (implemented in extended class)
-                return;
-            }
+            case ops.AND:
+                active = (and() != invert); // This is logical XOR
+                break;
+            case ops.OR:
+                active = (or() != invert);
+                break;
+            case ops.XOR:
+                active = (xor() != invert);
+                break;
         }
 
-        // All outputs are active, so output is active as well
-        active = true;
-        behavior(); // Call output behavior (implemented in extended class)
+        // Call output behavior (implemented in extended class)
+        behavior();
     }
 
     // Call this on Start, to connect all inputs to this output
@@ -46,7 +56,45 @@ public abstract class OutputLogic : MonoBehaviour
             input.Output = this;
         }
 
-        // Check start state
+        // Check starting state
         checkState();
+    }
+
+    // AND statement
+    private bool and()
+    {
+        // I like this lambda lol
+        return inputs.TrueForAll(x => x.active);
+    }
+
+    // OR statement
+    private bool or()
+    {
+        foreach (InputLogic input in inputs)
+        {
+            if (input.active)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // XOR statement
+    private bool xor()
+    {
+        int count = 0;
+        foreach (InputLogic input in inputs)
+        {
+            if (input.active)
+            {
+                count++;
+            }
+            if (count > 1)
+            {
+                return false;
+            }
+        }
+        return count == 1;
     }
 }
