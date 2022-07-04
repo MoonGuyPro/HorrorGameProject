095a6efbcdcdebaf78bcd4071f4e0f65feac5ca2
LiptonLeon <krystian.baraniecki@gmail.com>
2022-07-04 20:12:19 +0200

    feat: Player prefab

--- Horror/Assets/Scripts/Interactive/Lever.cs
+++ Horror/Assets/Scripts/Interactive/Lever.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Lever : InputLogic
 {
     private Animator animator;
+    private AudioSource s;
     public int changed;
 
     public void Start()
     {
         changed = 0;
         animator = transform.GetComponentInParent<Animator>();
+        s = GetComponent<AudioSource>();
     }
 
     protected override void behavior()
     {
         // Animate on state change
         animator.SetBool("active", active);
+        s.Play();
         changed ^= 1;
     }
 }
