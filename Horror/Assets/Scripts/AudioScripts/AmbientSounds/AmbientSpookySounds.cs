48791eb06abcfaa94f2ca721d42fae445a013f90
Maerorr <7hubert7@gmail.com>
2022-09-08 11:30:15 +0200

    change: re-write of AbstractRandomCall. Renamed to AbstractTimedCall. Uses Coroutines instead of counting frames. feat: AmbientSpookySounds scrript and files for randomly called spooky sounds

---
+++ Horror/Assets/Scripts/AudioScripts/AmbientSounds/AmbientSpookySounds.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace AudioScripts.AmbientSounds
+{
+    public class AmbientSpookySounds : AbstractRandomCall
+    {
+        [SerializeField] private AudioClip[] clips;
+        private AudioSource audioSource;
+        protected override void Start()
+        {
+            base.Start();
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        protected override void OnInterval()
+        {
+            print("Playing soundsss");
+            audioSource.clip = clips[Random.Range(0, clips.Length)];
+            audioSource.Play();
+        }
+    }
+}
