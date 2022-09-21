34be6a2065519ba50bb95632b8c5d657756694cd
Kris0211 <stawinogakris@gmail.com>
2022-09-21 23:51:41 +0200

    fix: SceneEntrance should rotate the player?

--- Horror/Assets/Scripts/Portal/SceneEntrance.cs
+++ Horror/Assets/Scripts/Portal/SceneEntrance.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SceneEntrance : MonoBehaviour
 {
 	public string lastExitName;
+	public bool overrideRotation = false;
+	public float xRotation, yRotation, zRotation;
 	
     // Start is called before the first frame update
     void Start()
@@ -12,7 +14,14 @@ public class SceneEntrance : MonoBehaviour
         if (PlayerPrefs.GetString("LastExitName") == lastExitName)
 		{
 			PlayerInstance.instance.transform.position = transform.position;
-			PlayerInstance.instance.transform.eulerAngles = transform.eulerAngles;
+			if (overrideRotation) 
+			{
+				PlayerInstance.instance.transform.rotation = Quaternion.Euler(new Vector3(xRotation, yRotation, zRotation));
+			}
+			else 
+			{
+				PlayerInstance.instance.transform.eulerAngles = transform.eulerAngles;
+			}
 		}
     }
 }
