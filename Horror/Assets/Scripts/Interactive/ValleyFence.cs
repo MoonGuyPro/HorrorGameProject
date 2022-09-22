b8508573b262b5ac829ebc4b1a75e6e01f122820
Maerorr <7hubert7@gmail.com>
2022-09-22 11:43:00 +0200

    feat: gate opening sounds

--- Horror/Assets/Scripts/Interactive/ValleyFence.cs
+++ Horror/Assets/Scripts/Interactive/ValleyFence.cs
@@ -6,20 +6,27 @@ using UnityEngine;
 public class ValleyFence : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource source;
     [Tooltip("This int sets how many sublevels need to be finished before this gate opens.")]
     public int gateNumber;
 
     private void Start()
     {
-        print(StaticGlobalVariables.GetGatesOpened());
         animator = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
         /*  
          * After player returns to the main valley lvl. This should be called to check if the player
          * managed to trigger an increment in the gatesopened variable possibly triggering opening of this gate.
          */
-        
-        if (StaticGlobalVariables.GetGatesOpened() >= gateNumber)
+
+        if (StaticGlobalVariables.GetGatesOpened() == gateNumber)
         {
+            animator.SetBool("active", true);
+            source.Play();
+        }
+        if (StaticGlobalVariables.GetGatesOpened() > gateNumber)
+        {
+            animator.speed = 5f;
             animator.SetBool("active", true);
         }
     }
