d9d6acfd05a2564c5d10e67d408ff13f062bbd14
Kris <stawinogakris@gmail.com>
2022-11-14 23:24:22 +0100

    fix: Portal cameras

--- Horror/Assets/Scripts/Portal/SceneEntrance.cs
+++ Horror/Assets/Scripts/Portal/SceneEntrance.cs
@@ -8,9 +8,29 @@ public class SceneEntrance : MonoBehaviour
 	public bool overrideRotation = false;
 	public float xRotation, yRotation, zRotation;
 	
+	Camera tempCamera;
+	MainCamera mainCam;
+	
     // Start is called before the first frame update
     void Start()
     {
+		//Debug.LogWarning("Gowno.");
+		//GameObject[] temp = GameObject.FindGameObjectsWithTag("MainCamera");
+		//mainCamera = temp[0].GetComponentInChildren<MainCamera>();
+		tempCamera = Camera.main;
+		//Debug.LogWarning("Kurwa.");
+		mainCam = tempCamera.GetComponent<MainCamera>();
+		if (mainCam) 
+		{
+			//Debug.LogWarning("Kupa.");
+			mainCam.FindPortals();
+		}
+		else
+		{
+			//Debug.LogWarning("Dupa.");
+		}
+		//Debug.LogWarning("Chuj.");
+
         if (PlayerPrefs.GetString("LastExitName") == lastExitName)
 		{
 			PlayerInstance.instance.transform.position = transform.position;
