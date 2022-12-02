8a3c2522f2a4344990500972e1ab90fb5ac547f9
LiptonLeon <krystian.baraniecki@gmail.com>
2022-12-02 10:46:56 +0100

    change: Temp scene change fix
    Disabled playerInstance singleton

--- Horror/Assets/Scripts/Legacy/PlayerInstance.cs
+++ Horror/Assets/Scripts/Legacy/PlayerInstance.cs
@@ -5,11 +5,14 @@ public class PlayerInstance : MonoBehaviour
 	public static PlayerInstance instance;
 	public static Transform playerCamera;
 	
+	// Krystian here -	I disabled singleton (inventory isn't kept between levels)
+	//					cuz I have no idea how to fix it rn xd
     void Start()
     {
-        if (instance != null) Destroy(gameObject);
+        /*if (instance != null) Destroy(gameObject);
 		else instance = this;
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject);*/
+        instance = this;
 		playerCamera = GetComponent<PlayerInteraction>().PlayerCamera;	
     }
 
