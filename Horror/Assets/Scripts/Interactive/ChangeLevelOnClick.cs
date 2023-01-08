56d4f4a3959855f7954be127b32a8c0a1dc0f8bc
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2023-01-08 17:07:57 +0100

    change: VideoPlayer is now a working level

---
+++ Horror/Assets/Scripts/Interactive/ChangeLevelOnClick.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevelOnClick : Interactive
+{
+    public string levelName;
+    
+    public override bool Interact(Inventory inv)
+    {
+        SceneManager.LoadScene(levelName);
+        return true;
+    }
+}
