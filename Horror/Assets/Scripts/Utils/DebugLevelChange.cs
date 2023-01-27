819995b433c48416dd03c5bcd5a4eba8fc262a89
LiptonLeon <krystian.baraniecki@gmail.com>
2023-01-27 16:48:50 +0100

    feat: DebugLevelChange
    Script attached to player, that provides shortcuts to quickly change levels

---
+++ Horror/Assets/Scripts/Utils/DebugLevelChange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Utils
+{
+    public class DebugLevelChange : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+                DebugChange("TreeLevel");
+            if (Input.GetKeyDown(KeyCode.P))
+                DebugChange("Caves");
+            if (Input.GetKeyDown(KeyCode.LeftBracket))
+                DebugChange("Labyrinth");
+            if (Input.GetKeyDown(KeyCode.RightBracket))
+                DebugChange("Walls");
+            if (Input.GetKeyDown(KeyCode.L))
+                DebugChange("Lights");
+            if (Input.GetKeyDown(KeyCode.Semicolon))
+                DebugChange("EasyMechanics");
+        }
+
+        private void DebugChange(string level)
+        {
+            Debug.Log("Debug level change to: " + level);
+            SceneManager.LoadScene(level);
+        }
+    }
+}
