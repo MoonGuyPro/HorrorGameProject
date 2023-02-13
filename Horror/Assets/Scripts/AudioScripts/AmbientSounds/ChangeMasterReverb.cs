a9ed23e1fd3cb4b250c17674c27030da2ca3d2b5
Maerorr <7hubert7@gmail.com>
2023-02-13 12:49:12 +0100

    feat: walls ambience sound, master reverb

--- Horror/Assets/Scripts/AudioScripts/AmbientSounds/ChangeMasterReverb.cs
+++ Horror/Assets/Scripts/AudioScripts/AmbientSounds/ChangeMasterReverb.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class ChangeMasterReverb : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float reverbValue = 0.75f;
     
     void Start()
     {
-        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MasterReverb", 0.75f);
+        if (reverbValue > 1.0f)
+        {
+            reverbValue = 1.0f;
+        }
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MasterReverb", reverbValue);
     }
 }
