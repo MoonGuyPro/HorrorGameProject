b17bde7ea6251fdd35661b5cb698503bc6e76926
Maerorr <7hubert7@gmail.com>
2023-02-21 11:00:30 +0100

    feat: cobe pillar drone and activation sounds

--- Horror/Assets/Scripts/Interactive/InputOutput/WeirdButton.cs
+++ Horror/Assets/Scripts/Interactive/InputOutput/WeirdButton.cs
@@ -1,12 +1,33 @@
+ using System;
+ using FMOD.Studio;
+ using FMODUnity;
  using UnityEngine;
  using UnityEngine.Serialization;
+ using STOP_MODE = FMOD.Studio.STOP_MODE;
 
  public class WeirdButton : InputLogic
  {
      public Animator cubeAnim;
+
+     [SerializeField] 
+     private EventReference activationSound;
      
+     [SerializeField] 
+     private EventReference droneSound;
+
+     private EventInstance droneInstance;
+
+     private void Start()
+     {
+         droneInstance = RuntimeManager.CreateInstance(droneSound);
+         droneInstance.set3DAttributes(RuntimeUtils.To3DAttributes(gameObject.transform));
+         droneInstance.start();
+     }
+
      protected override void Behavior()
      {
+         droneInstance.stop(STOP_MODE.ALLOWFADEOUT);
+         RuntimeManager.PlayOneShotAttached(activationSound, gameObject);
          cubeAnim.SetTrigger("Activated");
      }
  }
