d8958d31312210066654743f9925eec0e30647eb
Maerorr <7hubert7@gmail.com>
2023-02-21 11:59:11 +0100

    feat: root gate sound

--- Horror/Assets/Scripts/AudioScripts/PlayOnInteractive.cs
+++ Horror/Assets/Scripts/AudioScripts/PlayOnInteractive.cs
@@ -6,16 +6,28 @@ using UnityEngine;
 
 // THIS WAS CREATED FOR PURPOSES OF LABIRYNTHS
 // where there wasnt a single output logic but a multitude of them.
+
+// this also destroys
 public class PlayOnInteractive : OutputLogic
 {
     [SerializeField] private EventReference eventRef;
     [SerializeField] private List<GameObject> soundsToBeDestroyed = new List<GameObject>();
 
+    [SerializeField, Tooltip("Is the event reference to be spatialized? (if one doesnt work try the other ;) )")]
+    private bool isSpatial = false;
+
     protected override void Behavior()
     {
         if (active)
         {
-            RuntimeManager.PlayOneShot(eventRef);
+            if (isSpatial)
+            {
+                RuntimeManager.PlayOneShotAttached(eventRef, gameObject);
+            }
+            else
+            {
+                RuntimeManager.PlayOneShot(eventRef);
+            }
             foreach (var go in soundsToBeDestroyed)
             {
                 Destroy(go);
