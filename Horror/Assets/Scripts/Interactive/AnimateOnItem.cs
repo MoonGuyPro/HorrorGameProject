10e0809e98cbafedb9694ee026ad281665292c16
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2023-03-03 11:27:49 +0100

    feat: VineSappling logic

---
+++ Horror/Assets/Scripts/Interactive/AnimateOnItem.cs
@@ -0,0 +1,29 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+public class AnimateOnItem : Interactive
+{
+    private int changed;
+    
+    [Header("Required item name")]
+    public string itemName;
+    
+    [Header("Animator")]
+    public Animator anim;
+    public string boolName = "active";
+
+    [SerializeField] private EventReference plantSound;
+    public override bool Interact(Inventory inv)
+    {
+        if (inv.itemExists(itemName))
+        {
+            isActive = false;
+            inv.removeItem(itemName);
+            RuntimeManager.PlayOneShotAttached(plantSound, gameObject);
+            anim.SetBool(boolName, true);
+            
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Horror/Assets/Scripts/Pickable/VineSappling.cs
@@ -0,0 +1,11 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+public class VineSappling : Pickable
+{
+    public override void interact()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(pickUpSound, transform.position);
+        transform.position -= new Vector3(0, 10, 0);
+    }
+}
