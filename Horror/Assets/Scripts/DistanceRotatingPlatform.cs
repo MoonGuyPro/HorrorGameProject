d52284b8105ff26abf1b41d38d5e0b142b497a85
Kris <stawinogakris@gmail.com>
2023-03-09 23:00:17 +0100

    feat: DistanceRotatingPlatform

---
+++ Horror/Assets/Scripts/DistanceRotatingPlatform.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using FMODUnity;
+using UnityEngine;
+
+public class DistanceRotatingPlatform : MonoBehaviour
+{
+    private enum Angle
+    {
+        Pitch,
+        Yaw,
+        Roll
+    };
+    
+    [Tooltip("Range at which the platform triggers")]
+    public float triggerRange;
+
+    [Tooltip("Range at which the platform rotates back")]
+    public float deTriggerRange;
+
+    private Transform player;
+    private Quaternion defaultRotation;
+
+    private bool trigger = false;
+    private bool triggerPrevFrameValue = false;
+    private bool firstFrame = false;
+
+    [SerializeField, Tooltip("Time for the animation to fully finish")]
+    private float animationTime = 1.0f;
+
+    private float interpolator = 0.0f;
+    private float interpolationJump = 0.0f;
+    
+    [SerializeField]
+    private Angle eulerAngle = Angle.Pitch;
+    public float rotationAngle = 0.0f;
+
+    [SerializeField] 
+    private EventReference upSound;
+    [SerializeField]
+    private EventReference downSound;
+
+    void Start()
+    {
+        player = Camera.main.transform;
+        defaultRotation = transform.rotation;
+        interpolationJump = 1 / (100 * animationTime);
+        //transform.position = defaultPosition + Vector3.down * hideOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = (transform.position - player.position).magnitude;
+        //Debug.Log(distance);
+
+        if (distance <= triggerRange)
+        {
+            trigger = true;
+            if (triggerPrevFrameValue != trigger)
+            {
+                firstFrame = true;
+                interpolator = 0.0f;
+                RuntimeManager.PlayOneShotAttached(upSound, gameObject);
+            }
+        }
+
+        if (distance >= deTriggerRange)
+        {
+            trigger = false;
+            if (triggerPrevFrameValue != trigger)
+            {
+                firstFrame = true;
+                interpolator = 0.0f;
+                RuntimeManager.PlayOneShotAttached(downSound, gameObject);
+            }
+        }
+        triggerPrevFrameValue = trigger;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 newRotation = new Vector3(0, 0, 0);
+        if (trigger)
+        {
+            if (firstFrame)
+            {
+                interpolator = 0.0f;
+                firstFrame = false;
+            }
+            switch (eulerAngle)
+            {
+                case Angle.Pitch:
+                    newRotation.x = rotationAngle;
+                    break;
+                case Angle.Yaw:
+                    newRotation.y = rotationAngle ;
+                    break;
+                case Angle.Roll:
+                    newRotation.z = rotationAngle;
+                    break;
+            }
+            Vector3 lerpPosition = Vector3.Lerp(transform.eulerAngles, newRotation, interpolator);
+            interpolator += interpolationJump;
+            Clamp(ref interpolator, 0, 1);
+            transform.eulerAngles = lerpPosition;
+        }
+        else
+        {
+            if (firstFrame)
+            {
+                //Debug.Log("Fist frame of falling down");
+                interpolator = 0.0f;
+                firstFrame = false;
+            }
+            Vector3 lerpPosition = Vector3.Lerp(transform.eulerAngles, newRotation, interpolator);
+            interpolator += interpolationJump;
+            Clamp(ref interpolator, 0, 1);
+            transform.eulerAngles = lerpPosition;
+        }
+    }
+
+    private void Clamp(ref float x, float min, float max)
+    {
+        if (x < min)
+        {
+            x = min;
+        }
+        if (x > max)
+        {
+            x = max;
+        }
+    }
+}
