50c6428e44037db98651c46411235a0f9520f123
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2023-05-16 12:43:18 +0200

    dev: InteractiveShowHide

---
+++ Horror/Assets/Scripts/Interactive/InteractiveShowHide.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveShowHide : Interactive
+{
+    public List<GameObject> objectsToShowHide;
+
+    public override bool Interact(Inventory inv)
+    {
+        if (objectsToShowHide != null)
+        {
+                foreach(GameObject go in objectsToShowHide)
+                {
+                        go.SetActive(!go.active);
+                }       
+        }
+
+        return true;
+    }
+}
