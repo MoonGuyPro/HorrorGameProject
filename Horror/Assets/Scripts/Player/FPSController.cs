a78ab0def8412a363e3608c1260224fe36f71e22
Klaudia <alice4book@gmail.com>
2023-06-06 18:32:59 +0200

    fix: jumping
    - during jump, you can only rotate 90 degree - can't accelerate during a jump

--- Horror/Assets/Scripts/Player/FPSController.cs
+++ Horror/Assets/Scripts/Player/FPSController.cs
@@ -12,6 +12,7 @@ public class FPSController : PortalTraveller {
     public float smoothMoveTime = 0.1f;
     public float jumpForce = 8;
     public float gravity = 20;
+    public float maxYawJumping = 60;
 
     public bool lockCursor;
     public float mouseSensitivity = 10;
@@ -22,9 +23,12 @@ public class FPSController : PortalTraveller {
     Camera cam;
     public float yaw;
     public float pitch;
+    public float staringYaw;
     float smoothYaw;
     float smoothPitch;
 
+
+    float currentSpeed;
     float yawSmoothV;
     float pitchSmoothV;
     float verticalVelocity;
@@ -40,7 +44,6 @@ public class FPSController : PortalTraveller {
     bool jumping;
     float lastGroundedTime;
     bool disabled;
-    private int waitOneMove;
 
     // These events were created for audio triggering purposes.
     // Using this is much simpler then (as before) checking all parameters in Update()
@@ -76,52 +79,45 @@ public class FPSController : PortalTraveller {
         }
         smoothYaw = yaw;
         smoothPitch = pitch;
-        waitOneMove = 1;
+
+        currentSpeed = walkSpeed;
     }
 
-    void Update ()
+    void Update()
     {
         UpdatePrefs(); //todo: move it somewhere else to not call it every frame
-        if (Input.GetKeyDown (KeyCode.P)) 
+        if (Input.GetKeyDown(KeyCode.P))
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             Debug.Break();
         }
-        if (Input.GetKeyDown (KeyCode.O)) 
+        if (Input.GetKeyDown(KeyCode.O))
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             disabled = !disabled;
         }
 
-        if (disabled) 
+        if (disabled)
         {
             return;
         }
 
-        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
-        
-        // Uncomment to disable velocity change mid-air
-        // if (!jumping)
-        // {
-            Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
-        
-            Vector3 inputDir = new Vector3 (input.x, 0, input.y).normalized;
-            Vector3 worldInputDir = transform.TransformDirection (inputDir);
+        Vector2 input = new Vector2(0, Input.GetAxisRaw("Vertical")); // Set horizontal input to 0 during jumping
+        if (!jumping) // Allow horizontal movement during walking
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        }
 
-            Vector3 targetVelocity = worldInputDir * currentSpeed;
-            velocity = Vector3.SmoothDamp (velocity, targetVelocity, ref smoothV, smoothMoveTime);
-        // }else 
-        // {
-        //     if (waitOneMove == 0 && (controller.collisionFlags & CollisionFlags.Below) != 0) {
-        //         jumping = false;
-        //         waitOneMove = 2;            
-        //     }
-        //     waitOneMove--;
-        // }
-        
-        if (velocity.magnitude > 1f && !jumping && !bStuck) 
+        Vector3 inputDir = new Vector3(input.x, 0, input.y).normalized;
+        Vector3 worldInputDir = transform.TransformDirection(inputDir);
+
+        Vector3 targetVelocity = worldInputDir * currentSpeed;
+        velocity = Vector3.SmoothDamp(velocity, targetVelocity, ref smoothV, smoothMoveTime);
+
+        if (velocity.magnitude > 1f && !jumping && !bStuck)
         {
             if (currentSpeed > runSpeed - 0.1)
             {
@@ -139,10 +135,8 @@ public class FPSController : PortalTraveller {
         }
 
         verticalVelocity -= gravity * Time.deltaTime;
-        velocity = new Vector3 (velocity.x, verticalVelocity, velocity.z);
+        velocity = new Vector3(velocity.x, verticalVelocity, velocity.z);
 
-
-        // Stop player if he's stuck
         if (!bStuck)
         {
             var flags = controller.Move(velocity * Time.deltaTime);
@@ -155,16 +149,18 @@ public class FPSController : PortalTraveller {
             }
         }
 
-        if (Input.GetKeyDown (KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             float timeSinceLastTouchedGround = Time.time - lastGroundedTime;
-            if (controller.isGrounded || (!jumping && timeSinceLastTouchedGround < 0.15f)) {
+            if (controller.isGrounded || (!jumping && timeSinceLastTouchedGround < 0.15f))
+            {
+                staringYaw = yaw;
                 jumping = true;
                 verticalVelocity = jumpForce;
             }
         }
-
-        float mX = Input.GetAxisRaw ("Mouse X");
-        float mY = Input.GetAxisRaw ("Mouse Y");
+        float mX = Input.GetAxisRaw("Mouse X");
+        float mY = Input.GetAxisRaw("Mouse Y");
 
         // Verrrrrry gross hack to stop camera swinging down at start
         // Kris here - mMag can reach very high values at low framerate.
@@ -173,21 +169,26 @@ public class FPSController : PortalTraveller {
             mX = 0;
             mY = 0;
         } */
-        
-        if (PauseMenu.IsPaused) return;
-        
-        float rotSensitivity = mouseSensitivity * sensitivity;
-    
-        yaw += invertX ? -mX * rotSensitivity : mX * rotSensitivity;
-        pitch -= invertY ? -mY * rotSensitivity : mY * rotSensitivity;
-        pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
-        smoothPitch = Mathf.SmoothDampAngle (smoothPitch, pitch, ref pitchSmoothV, rotationSmoothTime);
-        smoothYaw = Mathf.SmoothDampAngle (smoothYaw, yaw, ref yawSmoothV, rotationSmoothTime);
 
-        transform.eulerAngles = Vector3.up * yaw;
-        cam.transform.localEulerAngles = Vector3.right * pitch;
-        
+        if (PauseMenu.IsPaused) return;
+
+        float rotSensitivity = mouseSensitivity * sensitivity;
+
+         yaw += invertX ? -mX * rotSensitivity : mX * rotSensitivity;
+        if (jumping) // Limit rotation around y-axis to 30 degrees during jumping
+        {
+            yaw = Mathf.Clamp(yaw, staringYaw - 30f, staringYaw + 30f); // Limit the yaw rotation to 30 degrees
+        }
+
+        pitch -= invertY ? -mY * rotSensitivity : mY * rotSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        smoothPitch = Mathf.SmoothDampAngle(smoothPitch, pitch, ref pitchSmoothV, rotationSmoothTime);
+        smoothYaw = Mathf.SmoothDampAngle(smoothYaw, yaw, ref yawSmoothV, rotationSmoothTime);
+
+        transform.eulerAngles = Vector3.up * smoothYaw;
+        cam.transform.localEulerAngles = Vector3.right * smoothPitch;
     }
+
 
     public override void Teleport (Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot) {
         transform.position = pos;
