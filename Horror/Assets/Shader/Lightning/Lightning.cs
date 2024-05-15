da7e35f4663d0819d916269875df8bcfd841290b
Klaudia <alice4book@gmail.com>
2024-05-15 20:57:13 +0200

    fix: OnValidate() in Lightning.cs

--- Horror/Assets/Shader/Lightning/Lightning.cs
+++ Horror/Assets/Shader/Lightning/Lightning.cs
@@ -82,4 +82,47 @@ public class Lightning : MonoBehaviour
             }
         }
     }
+
+    private void OnValidate()
+    {
+        if (lightningParticleSystem != null)
+        {
+            var rend = lightningParticleSystem.GetComponent<ParticleSystemRenderer>();
+            material = rend.material;
+            material.SetColor("_Color", lightningColor);
+            var main = lightningParticleSystem.main;
+            main.startLifetime = lightningLifespawn;
+            main.duration = space + lightningLifespawn;
+        }
+        if (flashParticleSystem != null)
+        {
+            var main = flashParticleSystem.main;
+            main.startColor = flashColor;
+            main.startLifetime = flashLifespawn;
+            main.duration = space + lightningLifespawn;
+        }
+        if (flashOnImpactParticleSystem != null)
+        {
+            var main = flashOnImpactParticleSystem.main;
+            main.startColor = flashColor;
+            main.startLifetime = flashLifespawn;
+            main.startDelay = lightningLifespawn - flashLifespawn;
+            main.duration = space + lightningLifespawn;
+        }
+        if (glowParticleSystem != null)
+        {
+            var main = glowParticleSystem.main;
+            main.startColor = flashColor;
+            main.startLifetime = glowLifespawn;
+            main.duration = space + lightningLifespawn;
+        }
+        if (sparksParticleSystem != null)
+        {
+            var main = sparksParticleSystem.main;
+            main.startColor = flashColor;
+            main.startLifetime = new ParticleSystem.MinMaxCurve(sparksLifespawnMin, sparksLifespawnMax);//Random.Range(sparksLifespawnMin, sparksLifespawnMax);
+            main.startDelay = lightningLifespawn - flashLifespawn;
+            main.duration = space + lightningLifespawn;
+        }
+    }
 }
