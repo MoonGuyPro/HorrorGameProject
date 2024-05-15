b1cfa7d5cb639df32cdadcbe8dbcf96c42c8b36f
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2024-05-15 13:04:33 +0200

    feat: Pressing any key in cutscene

--- Horror/Assets/Scripts/UI/ComicCutscene.cs
+++ Horror/Assets/Scripts/UI/ComicCutscene.cs
@@ -1,13 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class ComicCutscene : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] TextMeshProUGUI bottomText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bottomText.enabled = false;
+
+        InputActionAsset inputActionAsset = Resources.Load<InputActionAsset>("NyctoInputActions");
+		InputActionMap inputActionMap = inputActionAsset.FindActionMap("UI");
+        inputActionMap.FindAction("Any").performed += OnContinue;
+    }
+
+    private void OnContinue(InputAction.CallbackContext context)
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        {
+            animator.speed = 1.0f;
+            bottomText.enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -16,8 +36,9 @@ public class ComicCutscene : MonoBehaviour
         
     }
 
-    void OnTest()
+    void OnBreak()
     {
-        
+        animator.speed = 0.0f;
+        bottomText.enabled = true;
     }
 }
