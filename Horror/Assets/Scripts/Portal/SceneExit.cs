8f55a7d3dc9548d173a9fb0be0fd1adbbc3c9aff
Kris <stawinogakris@gmail.com>
2024-05-18 19:39:36 +0200

    feat: scene transition

--- Horror/Assets/Scripts/Portal/SceneExit.cs
+++ Horror/Assets/Scripts/Portal/SceneExit.cs
@@ -1,18 +1,44 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneExit : MonoBehaviour
 {
-	public string targetScene;
-	public string exitName;
+	public string targetSceneName;
+	//public string exitName;
+
+	public float transitionTime = 1.0f;
+	
+	[ColorUsage(false)] public Color transitionColor = Color.white;
+
+	public GameObject playerTransition;
+
+	private Animator transitionAnim;
+	private RawImage rawImage;
+
+	void Awake()
+	{
+		transitionAnim = playerTransition.GetComponent<Animator>();
+		rawImage = playerTransition.GetComponent<RawImage>();
+		rawImage.color = transitionColor;
+	}
 	
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.CompareTag("Player")) { // Only players can use SceneExit portals
-			PlayerPrefs.SetString("LastExitName", exitName);
-			SceneManager.LoadScene(targetScene);
+		if (other.gameObject.CompareTag("Player")) // Only player can use SceneExits
+		{ 
+			//PlayerPrefs.SetString("LastExitName", exitName);
+			StartCoroutine(LoadLevel(targetSceneName));
 		}
+	}
+
+	IEnumerator LoadLevel(string levelName)
+	{
+		transitionAnim.SetTrigger("Start");
+
+		yield return new WaitForSeconds(transitionTime);
+
+		SceneManager.LoadScene(levelName);
 	}
 }
