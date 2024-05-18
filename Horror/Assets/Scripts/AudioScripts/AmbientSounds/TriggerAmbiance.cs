77f17592d3f325318b8b94cea99e51ad20546045
Maerorr <7hubert7@gmail.com>
2024-05-18 13:25:27 +0200

    feat: new labirynth solve sounds

--- Horror/Assets/Scripts/AudioScripts/AmbientSounds/TriggerAmbiance.cs
+++ Horror/Assets/Scripts/AudioScripts/AmbientSounds/TriggerAmbiance.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
@@ -10,49 +7,22 @@ using STOP_MODE = FMOD.Studio.STOP_MODE;
 public class TriggerAmbiance : MonoBehaviour
 {
     
-    [SerializeField, FormerlySerializedAs("windSound")] private EventReference eventReference;
+    [SerializeField] private EventReference eventReference;
     private EventInstance instance;
 
     [SerializeField] 
     private bool playAtStart;
 
     private bool isPlaying = false;
-    private int triggerCooldown = 0;
-    private void Start()
-    {
-        instance = RuntimeManager.CreateInstance(eventReference);
-        //instance.start();
-        if (playAtStart)
-        {
-            instance.start();
-            isPlaying = true;
-        }
-    }
 
-    private void Update()
-    {
-        if (triggerCooldown > 0)
-        {
-            triggerCooldown--;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (triggerCooldown == 0)
+        if (!isPlaying)
         {
-            if (isPlaying)
-            {
-                instance.stop(STOP_MODE.ALLOWFADEOUT);
-                isPlaying = false;
-            }
-            else
-            {
-                instance.start();
-                isPlaying = true;
-            }
-
-            triggerCooldown = 30;
+            instance = RuntimeManager.CreateInstance(eventReference);
+            instance.start();
+            isPlaying = true;
         }
     }
 
