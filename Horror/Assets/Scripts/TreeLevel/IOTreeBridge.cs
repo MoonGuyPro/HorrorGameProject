9e558cbfc4e66e4b9311687cb74ddd04cf3db5d9
Maeror <7hubert7@gmail.com>
2025-03-12 10:20:04 +0100

    feat: caves rock explosion + buttons rewiring

--- Horror/Assets/Scripts/TreeLevel/IOTreeBridge.cs
+++ Horror/Assets/Scripts/TreeLevel/IOTreeBridge.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using FMODUnity;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -7,11 +8,13 @@ public class IOTreeBridge : MonoBehaviour
 {
     [SerializeField] PlayableDirector playableDirector;
     [SerializeField] float playbackSpeed = 1.0f;
+    [SerializeField] EventReference eventReference;
 
     public void OnInteraction()
     {
         playableDirector.RebuildGraph();
         playableDirector.playableGraph.GetRootPlayable(0).SetSpeed(playbackSpeed);
         playableDirector.Play();
+        RuntimeManager.PlayOneShot(eventReference, transform.position);
     }
 }
